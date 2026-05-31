1bb126e74505fd1bd39f8eabe65a9f52ab78309f
Chrisimade <imadetradesinvestment@gmail.com>
2026-05-31 04:39:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const pool = require('./db');
 require('dotenv').config();
@@ -14,6 +15,11 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'admin.html'));
+});
 
 // ── TEST ROUTE ──
 app.get('/', (req, res) => {
